ef696edbfe03febb671ff63af5ff33270bf0da5b
Nick <pattison.nick@gmail.com>
2019-02-22 11:08:47 -0500

    Organized scripts
    Also added LifeSpawner class (empty). This behavior should be loaded onto our voxel chunks.

---
+++ Assets/Scripts/ChunkSpawning/LifeSpawner.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(VoxelChunk))]
+public class LifeSpawner : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
