5bc73e1eec79ff70595b74cc8ffc835de0d6ae4b
Unknown <kaykberry@gmail.com>
2019-03-01 13:52:46 -0500

    added in comments
    +comments to branchspawner
    +comments added to branchsegment

---
+++ Assets/Scripts/KKB_BranchSegment.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KKB_BranchSegment : MonoBehaviour
+{
+    /* endpoint set so that the objects will spawn where the endpoint is set */
+    public Transform endPoint;
+}
