023f2db28a0b6eff9a84f2b2564c33f4a1ba2522
Nick <pattison.nick@gmail.com>
2019-03-03 19:07:25 -0500

    Commented the code!

--- Assets/Scripts/ChunkSpawning/LifeSpawner.cs
+++ Assets/Scripts/ChunkSpawning/LifeSpawner.cs
@@ -2,22 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This behavior is responsible for spawning prefabs on a VoxelChunk. It's currently being used to spawn coral, but could be used to spawn anything, really.
+/// </summary>
 [RequireComponent(typeof(VoxelChunk))]
 [RequireComponent(typeof(MeshFilter))]
-
 public class LifeSpawner : MonoBehaviour
 {
-    /// <summary>
-    /// Socket for Dom's Vornoi Coral 
-    /// </summary>
-    public GameObject Prefab_Voronoi_Coral;
 
+    /// <summary>
+    /// The possible Biomes our game supports
+    /// </summary>
     public enum BiomeOwner
     {
         Andrew,
         Cameron,
-        Christopher,
-        Dominc,
+        Chris,
+        Dominic,
         Eric,
         Jess,
         Jesse,
@@ -29,48 +30,99 @@ public class LifeSpawner : MonoBehaviour
         Zach
     }
 
+    /// <summary>
+    /// This struct contains one value: BiomeOwner. It also contains several convenience methods for converting from color to biome and vice versa.
+    /// </summary>
     public struct Biome
     {
+        /// <summary>
+        /// How many biomes the game currently supports.
+        /// </summary>
         public static int COUNT = System.Enum.GetNames(typeof(BiomeOwner)).Length;
+        /// <summary>
+        /// Whose biome this is
+        /// </summary>
         public BiomeOwner owner;
+        /// <summary>
+        /// Gets a hue value to use for vertex color
+        /// </summary>
+        /// <returns>A value from 0.0 to 1.0</returns>
         public float GetHue()
         {
             return ((int)owner) / ((float)COUNT);
         }
+        /// <summary>
+        /// Gets a Color value to use for vertex color
+        /// </summary>
+        /// <returns></returns>
         public Color GetVertexColor()
         {
             return Color.HSVToRGB(GetHue(), 1, 1);
         }
-        public Biome(BiomeOwner num)
+        /// <summary>
+        /// Creates a Biome associated with a specified BiomeOwner
+        /// </summary>
+        /// <param name="owner">The BiomeOwner who for this Biome</param>
+        public Biome(BiomeOwner owner)
         {
-            this.owner = num;
+            this.owner = owner;
         }
+        /// <summary>
+        /// Creates a Biome from a specified Color
+        /// </summary>
+        /// <param name="color">The color to use. In our case, this will be a vertex color stored in a mesh.</param>
+        /// <returns>A Biome</returns>
         public static Biome FromColor(Color color)
         {
             Color.RGBToHSV(color, out float h, out float s, out float v);
             return FromHue(h);
         }
+        /// <summary>
+        /// Creates a Biome from a specified hue value
+        /// </summary>
+        /// <param name="hue">The hue value to use. Should be 0.0 to 1.0</param>
+        /// <returns>A Biome</returns>
         public static Biome FromHue(float hue)
         {
             int num = Mathf.RoundToInt(hue * COUNT);
             return new Biome((BiomeOwner)num);
         }
+        /// <summary>
+        /// Creates Biome from a specified integer.
+        /// </summary>
+        /// <param name="i">This integer should correspond to an index value in BiomeOwner</param>
+        /// <returns>A Biome</returns>
         public static Biome FromInt(int i)
         {
             return new Biome((BiomeOwner)i);
         }
     }
-
+    /// <summary>
+    /// The minimum amount of life to spawn.
+    /// </summary>
     public int lifeAmountMin = 1;
+    /// <summary>
+    /// The maximum amount of life to spawn.
+    /// </summary>
     public int lifeAmountMax = 5;
 
 
+    /// <summary>
+    /// Socket for Dom's Vornoi Coral 
+    /// </summary>
+    public GameObject prefabCoralVoronoi;
     public GameObject prefabCoralBroccoli;
     public GameObject prefabCoralTree;
     public GameObject prefabCoralCrystal;
 
+    /// <summary>
+    /// The MeshFilter that's (hopefully) loaded onto this VoxelChunk 
+    /// </summary>
     MeshFilter mesh;
 
+    /// <summary>
+    /// Attempt to spawn a bunch of life on this VoxelChunk
+    /// </summary>
     public void SpawnSomeLife()
     {
         mesh = GetComponent<MeshFilter>();
@@ -118,7 +170,7 @@ public class LifeSpawner : MonoBehaviour
         //if (biome.owner == BiomeOwner.Andrew) prefab = ;
         if (biome.owner == BiomeOwner.Cameron) prefab = prefabCoralCrystal;
         //if (biome.owner == BiomeOwner.Christopher) prefab = ;
-        if (biome.owner == BiomeOwner.Dominc) prefab = Prefab_Voronoi_Coral;
+        if (biome.owner == BiomeOwner.Dominic) prefab = prefabCoralVoronoi;
         if (biome.owner == BiomeOwner.Eric) prefab = prefabCoralTree;
         if (biome.owner == BiomeOwner.Jess) prefab = prefabCoralBroccoli;
         //if (biome.owner == BiomeOwner.Jesse) prefab = ;
@@ -134,6 +186,13 @@ public class LifeSpawner : MonoBehaviour
         return true;
     }
 
+    /// <summary>
+    /// Spawns a specific prefab
+    /// </summary>
+    /// <param name="prefab">The prefab to spawn</param>
+    /// <param name="position">The world position to spawn the prefab</param>
+    /// <param name="rotation">The world rotation to use when spawning the prefab</param>
+    /// <param name="scale">The local scale to spawn the prefab</param>
     void SpawnPrefab(GameObject prefab, Vector3 position, Quaternion? rotation = null, float scale = 1)
     {
         Quaternion rot = (rotation != null) ? (Quaternion)rotation : Quaternion.identity;
