7b3f44650e167de5eb7e7b5da475aa7822c857e0
Kyle Lowery <Loweryk1@ferris.edu>
2019-04-05 16:32:20 -0400

    Added a GameObject to test TaperCube code.

--- Assets/Scripts/MeshTools.cs
+++ Assets/Scripts/MeshTools.cs
@@ -418,4 +418,143 @@ public static class MeshTools
         mesh.SetTriangles(tris, 0);
         return mesh;
     }
+
+    /// <summary>
+    /// This function generates and returns a 1m cube mesh that tapers at its end. The anchor point is the back center of the mesh.
+    /// </summary>
+    /// <returns>A mesh object with normals and uvs. No color information has been set.</returns>
+    public static Mesh MakeTaperedCube()
+    {
+        List<Vector3> verts = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
+        List<Vector3> normals = new List<Vector3>();
+        List<int> tris = new List<int>();
+
+        // Front face
+        verts.Add(new Vector3(-0.5f, 0, -0.5f));
+        verts.Add(new Vector3(-0.5f, 1, -0.5f));
+        verts.Add(new Vector3(+0.5f, 1, -0.5f));
+        verts.Add(new Vector3(+0.5f, 0, -0.5f));
+        normals.Add(new Vector3(0, 0, -1));
+        normals.Add(new Vector3(0, 0, -1));
+        normals.Add(new Vector3(0, 0, -1));
+        normals.Add(new Vector3(0, 0, -1));
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+        tris.Add(0);
+        tris.Add(1);
+        tris.Add(2);
+        tris.Add(2);
+        tris.Add(3);
+        tris.Add(0);
+
+        // Back face
+        verts.Add(new Vector3(-0.5f, 0, +0.5f));
+        verts.Add(new Vector3(+0.5f, 0, +0.5f));
+        verts.Add(new Vector3(+0.5f, 1, +0.5f));
+        verts.Add(new Vector3(-0.5f, 1, +0.5f));
+        normals.Add(new Vector3(0, 0, +1));
+        normals.Add(new Vector3(0, 0, +1));
+        normals.Add(new Vector3(0, 0, +1));
+        normals.Add(new Vector3(0, 0, +1));
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+        tris.Add(4);
+        tris.Add(5);
+        tris.Add(6);
+        tris.Add(6);
+        tris.Add(7);
+        tris.Add(4);
+
+        // Left face 
+        verts.Add(new Vector3(-0.5f, 0, -0.5f));
+        verts.Add(new Vector3(-0.5f, 0, +0.5f));
+        verts.Add(new Vector3(-0.5f, 1, +0.5f));
+        verts.Add(new Vector3(-0.5f, 1, -0.5f));
+        normals.Add(new Vector3(-1, 0, 0));
+        normals.Add(new Vector3(-1, 0, 0));
+        normals.Add(new Vector3(-1, 0, 0));
+        normals.Add(new Vector3(-1, 0, 0));
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+        tris.Add(8);
+        tris.Add(9);
+        tris.Add(10);
+        tris.Add(10);
+        tris.Add(11);
+        tris.Add(8);
+
+        // Right face
+        verts.Add(new Vector3(+0.5f, 0, -0.5f));
+        verts.Add(new Vector3(+0.5f, 1, -0.5f));
+        verts.Add(new Vector3(+0.5f, 1, +0.5f));
+        verts.Add(new Vector3(+0.5f, 0, +0.5f));
+        normals.Add(new Vector3(+1, 0, 0));
+        normals.Add(new Vector3(+1, 0, 0));
+        normals.Add(new Vector3(+1, 0, 0));
+        normals.Add(new Vector3(+1, 0, 0));
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+        tris.Add(12);
+        tris.Add(13);
+        tris.Add(14);
+        tris.Add(14);
+        tris.Add(15);
+        tris.Add(12);
+
+        // Top face
+        verts.Add(new Vector3(-0.5f, 1, -0.5f));
+        verts.Add(new Vector3(-0.5f, 1, +0.5f));
+        verts.Add(new Vector3(+0.5f, 1, +0.5f));
+        verts.Add(new Vector3(+0.5f, 1, -0.5f));
+        normals.Add(new Vector3(0, +1, 0));
+        normals.Add(new Vector3(0, +1, 0));
+        normals.Add(new Vector3(0, +1, 0));
+        normals.Add(new Vector3(0, +1, 0));
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+        tris.Add(16);
+        tris.Add(17);
+        tris.Add(18);
+        tris.Add(18);
+        tris.Add(19);
+        tris.Add(16);
+
+        // Bottom face 
+        verts.Add(new Vector3(-0.5f, 0, -0.5f));
+        verts.Add(new Vector3(+0.5f, 0, -0.5f));
+        verts.Add(new Vector3(+0.5f, 0, +0.5f));
+        verts.Add(new Vector3(-0.5f, 0, +0.5f));
+        normals.Add(new Vector3(0, -1, 0));
+        normals.Add(new Vector3(0, -1, 0));
+        normals.Add(new Vector3(0, -1, 0));
+        normals.Add(new Vector3(0, -1, 0));
+        uvs.Add(new Vector2(0, 0));
+        uvs.Add(new Vector2(0, 1));
+        uvs.Add(new Vector2(1, 1));
+        uvs.Add(new Vector2(1, 0));
+        tris.Add(20);
+        tris.Add(21);
+        tris.Add(22);
+        tris.Add(22);
+        tris.Add(23);
+        tris.Add(20);
+
+        Mesh mesh = new Mesh();
+        mesh.SetVertices(verts);
+        mesh.SetUVs(0, uvs);
+        mesh.SetNormals(normals);
+        mesh.SetTriangles(tris, 0);
+        return mesh;
+    }
 }
