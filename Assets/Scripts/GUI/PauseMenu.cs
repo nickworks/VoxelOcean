f3f3b314d18b5342e65205e3052ecf1b58690a56
Cameron Garchow <camerong124@gmail.com>
2019-04-08 11:24:17 -0500

    Pause Menu Functionality
    Added The Pause Functionality
    Pause Now works, but there is very little to be effected right now.
    
    Will need to look at how to fix this error : https://docs.microsoft.com/en-us/dotnet/csharp/language-reference/compiler-messages/cs0115
    
    Once that is fixed will be able to load scenes.
    
    Having trouble loading the scenes in editor. No main menu yet so nowhere to load.

---
+++ Assets/Scripts/GUI/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GamePaused = false;
+    public GameObject pausemenuUI;
+    public string OBJ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+    public void Resume()
+    {
+        pausemenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+    void Pause()
+    {
+        pausemenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("OBJ");
+        Time.timeScale = 1f;
+    }
+    public void QuitGame()
+    {
+
+        Debug.Log("Quit Game");
+    }
+}
