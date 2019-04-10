ac522b41a2063fb4cc4b3e2b1c5392e8a904c438
AJ <Smithja@ferris.edu>
2019-04-10 16:38:24 -0400

    Small amount of progress made
    TODO: Continue to rough out menu
    TODO: Continue to program menu interactions
    + changed the way that menu objects are spawned

--- Assets/UIController.cs
+++ Assets/UIController.cs
@@ -8,10 +8,33 @@ public class UIController : MonoBehaviour
 {
     public GameObject titleText;
     public GameObject pressAnyKeyText;
+
+    public Transform newTitleTextTransform;
+    public Transform playTextTransform;
+    public Transform creditsTextTransform;
+    public Transform optionsTextTransform;
+    public Transform quitTextTransform;
+
+    public GameObject playText;
+    public GameObject creditsText;
+    public GameObject optionsText;
+    public GameObject quitText;
+
+    private bool anyKeyPressed;
+
+    
+
+    private bool transitionToSecondaryMenu = false;
+
+    private float startTime = 0;
+   
+    public float transitionSpeed;
+    private float length;
     // Start is called before the first frame update
     void Start()
     {
-        
+        startTime = Time.time;
+        length = Vector3.Distance(titleText.transform.position, newTitleTextTransform.position);
     }
 
     // Update is called once per frame
@@ -19,16 +42,49 @@ public class UIController : MonoBehaviour
     {
        if(Input.GetMouseButtonDown(0))
         {
-            print("here");
-            //RaycastHit hit;
-            // if(Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, 10000) )
+            
+            
+            
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            if(Physics.Raycast(ray, 100))
+            RaycastHit hit;
+            if(Physics.Raycast(ray,out hit, 100))
             {
-
+                if(hit.transform == pressAnyKeyText.transform)
+                {
+                    print("we are going to alter main menu now");
+                    //We call the alter menu function
+                    AlterMenu();
+                }
                 Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward), Color.red);
-                Debug.Log("GOT A HIT");
+               
             }//End of if Raycast condition
+
+
+            //USED TO MOVE PAUSE MENU UP
+            if(transitionToSecondaryMenu)
+            {
+                float distance = (Time.time - startTime) * transitionSpeed;
+
+               // float journey = distance / length;
+                titleText.transform.position = Vector3.Lerp(titleText.transform.position, newTitleTextTransform.position, Time.deltaTime);
+               
+                    CreateSecondaryMenu();
+                
+            }//end of transitiontosecondmenu condition
+
         }//End of Input condition
+    }
+
+
+    void AlterMenu()
+    {
+        //Destroy(pressAnyKeyText);
+        pressAnyKeyText.SetActive(false);
+        transitionToSecondaryMenu = true;
+    }
+
+    void CreateSecondaryMenu()
+    {
+        playText.SetActive(true);
     }
 }
