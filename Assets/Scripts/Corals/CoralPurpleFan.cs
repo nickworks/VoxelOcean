2ddde663a5c4739830f19671c12c5f465a438d0e
Unknown <kaykberry@gmail.com>
2019-04-11 23:02:47 -0400

    Updated Coral Purple Fan
    +fixed weird merge confilicts

--- Assets/Scripts/Corals/CoralPurpleFan.cs
+++ Assets/Scripts/Corals/CoralPurpleFan.cs
@@ -11,10 +11,7 @@ public class CoralPurpleFan : MonoBehaviour
     /// </summary>
     public int iterations;
     public int iteratoinsFan;
-<<<<<<< HEAD
     public bool sideFan = false;
-=======
->>>>>>> PurpleFanUpdate
     /// <summary>
     /// How the scaling of the branches are for both the skeleton set and fan set
     /// </summary>
@@ -31,13 +28,10 @@ public class CoralPurpleFan : MonoBehaviour
     /// </summary>
     public void Build()
     {
-<<<<<<< HEAD
+
         iterations = Random.Range(3, 5);
         iteratoinsFan = iterations;
-=======
-        iterations = Random.Range(2, 4);
-        iteratoinsFan = iterations + 1; 
->>>>>>> PurpleFanUpdate
+
         List<CombineInstance> meshes = new List<CombineInstance>();
         Grow(iterations, meshes, Vector3.zero, Quaternion.identity, 1);
         Grow2(iteratoinsFan, meshes, Vector3.zero, Quaternion.identity, 1);
@@ -68,18 +62,12 @@ public class CoralPurpleFan : MonoBehaviour
 
 
         meshes.Add(inst);
-<<<<<<< HEAD
-
-=======
-      
->>>>>>> PurpleFanUpdate
 
         //where the branches will be positioned when spawning in
         pos = inst.transform.MultiplyPoint(new Vector3(0, 1, 0));
 
         //rotation of the branches spawned
         Quaternion rot1 = rot * Quaternion.Euler(0, 0, Random.Range(30, 60));
-<<<<<<< HEAD
         Quaternion rot2 = rot * Quaternion.Euler(Random.Range(10, 40), Random.Range(10, 40), 0);
         Quaternion rot3 = rot * Quaternion.Euler(Random.Range(-40, -10), Random.Range(-40, -10), 0);
 
@@ -90,26 +78,8 @@ public class CoralPurpleFan : MonoBehaviour
         Grow(num, meshes, pos, rot1, scale);
         Grow(num, meshes, pos, rot2, scale);
         Grow(num, meshes, pos, rot3, scale);
-
-
-
-
     }
-=======
-        Quaternion rot2 = rot * Quaternion.Euler(0, 0, Random.Range(-60, -30));
-        Quaternion rot3 = rot * Quaternion.Euler(0, 0, Random.Range(-10, 20));
 
-        //how each iteration of a branch will be generated
-        scale *= .8f;
-        //branches that spawn for the skeletal part of the coral
-
-        Grow(num, meshes, pos, rot1, scale);
-        Grow(num, meshes, pos, rot2, scale);
-        Grow(num, meshes, pos, rot3, scale);
-
-        
-    }
->>>>>>> PurpleFanUpdate
     private void Grow2(int num, List<CombineInstance> meshes, Vector3 pos, Quaternion rot, float scale)
     {
         if (num <= 0) return; //stop recursive function
@@ -119,14 +89,9 @@ public class CoralPurpleFan : MonoBehaviour
 
         coralBase.mesh = MakeCube1();
         coralBase.transform = Matrix4x4.TRS(pos, rot, baseScaling * scale);
-<<<<<<< HEAD
-=======
-
->>>>>>> PurpleFanUpdate
 
         meshes.Add(coralBase);
 
-<<<<<<< HEAD
         meshes.Add(coralBase);
 
 
@@ -136,26 +101,13 @@ public class CoralPurpleFan : MonoBehaviour
         pos = coralBase.transform.MultiplyPoint(new Vector3(0, 1f, 0));
         Quaternion rot1 = rot * Quaternion.Euler(0, 0, Random.Range(40, 80));
         Quaternion rot2 = rot * Quaternion.Euler(0, 0, Random.Range(-80, -40));
-        Quaternion rot3 = rot * Quaternion.Euler(Random.Range(10, 40), Random.Range(10,40), 0);
+        Quaternion rot3 = rot * Quaternion.Euler(Random.Range(10, 40), Random.Range(10, 40), 0);
         Quaternion rot4 = rot * Quaternion.Euler(Random.Range(-40, -10), Random.Range(-40, -10), 0);
 
         Grow2(num, meshes, pos, rot1, scale);
         Grow2(num, meshes, pos, rot2, scale);
         Grow2(num, meshes, pos, rot3, scale);
         Grow2(num, meshes, pos, rot4, scale);
-=======
-
-        //how each iteration of a branch will be generated
-        scale *= .8f;
-        //branches that spawn for the skeletal part of the coral
-        pos = coralBase.transform.MultiplyPoint(new Vector3(0, 1f, 0));
-        Quaternion rot1 = rot * Quaternion.Euler(0, 0, Random.Range(40, 80));
-        Quaternion rot2 = rot * Quaternion.Euler(0, 0, Random.Range(-80, -40));
->>>>>>> PurpleFanUpdate
-
-        Grow2(num, meshes, pos, rot1, scale);
-        Grow2(num, meshes, pos, rot2, scale);
-        Grow2(num, meshes, pos, rot, scale);
 
 
     }
@@ -290,165 +242,6 @@ public class CoralPurpleFan : MonoBehaviour
         tris.Add(23);
         tris.Add(20);
 
-        
-
-        foreach (Vector3 pos in verts)
-        {
-
-
-            Color color = Color.HSVToRGB(1, 1, 1);
-
-            colors.Add(color);
-        }
-
-        Mesh mesh = new Mesh();
-        mesh.SetVertices(verts);
-        mesh.SetUVs(0, uvs);
-        mesh.SetNormals(normals);
-        mesh.SetTriangles(tris, 0);
-        mesh.SetColors(colors);
-        return mesh;
-
-    }
-    private Mesh MakeCube1()
-    {
-        List<Vector3> verts = new List<Vector3>();
-        List<Vector3> uvs = new List<Vector3>();
-        List<Vector3> normals = new List<Vector3>();
-        List<Color> colors = new List<Color>();
-        List<int> tris = new List<int>();
-
-        //Front
-        verts.Add(new Vector3(-0.5f, 0, -0.5f));
-        verts.Add(new Vector3(-0.5f, 1, -0.5f));
-        verts.Add(new Vector3(+0.5f, 1, -0.5f));
-        verts.Add(new Vector3(+0.5f, 0, -0.5f));
-        normals.Add(new Vector3(0, 0, -1));
-        normals.Add(new Vector3(0, 0, -1));
-        normals.Add(new Vector3(0, 0, -1));
-        normals.Add(new Vector3(0, 0, -1));
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 0));
-        tris.Add(0);
-        tris.Add(1);
-        tris.Add(2);
-        tris.Add(2);
-        tris.Add(3);
-        tris.Add(0);
-
-        //Back
-        verts.Add(new Vector3(-0.5f, 0, +0.5f));
-        verts.Add(new Vector3(+0.5f, 0, +0.5f));
-        verts.Add(new Vector3(+0.5f, 1, +0.5f));
-        verts.Add(new Vector3(-0.5f, 1, +0.5f));
-        normals.Add(new Vector3(0, 0, +1));
-        normals.Add(new Vector3(0, 0, +1));
-        normals.Add(new Vector3(0, 0, +1));
-        normals.Add(new Vector3(0, 0, +1));
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 0));
-        tris.Add(4);
-        tris.Add(5);
-        tris.Add(6);
-        tris.Add(6);
-        tris.Add(7);
-        tris.Add(4);
-
-        //Left
-        verts.Add(new Vector3(-0.5f, 0, -0.5f));
-        verts.Add(new Vector3(-0.5f, 0, +0.5f));
-        verts.Add(new Vector3(-0.5f, 1, +0.5f));
-        verts.Add(new Vector3(-0.5f, 1, -0.5f));
-        normals.Add(new Vector3(-1, 0, 0));
-        normals.Add(new Vector3(-1, 0, 0));
-        normals.Add(new Vector3(-1, 0, 0));
-        normals.Add(new Vector3(-1, 0, 0));
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 0));
-        tris.Add(8);
-        tris.Add(9);
-        tris.Add(10);
-        tris.Add(10);
-        tris.Add(11);
-        tris.Add(8);
-
-        //Right
-        verts.Add(new Vector3(+0.5f, 0, -0.5f));
-        verts.Add(new Vector3(+0.5f, 1, -0.5f));
-        verts.Add(new Vector3(+0.5f, 1, +0.5f));
-        verts.Add(new Vector3(+0.5f, 0, +0.5f));
-        normals.Add(new Vector3(+1, 0, 0));
-        normals.Add(new Vector3(+1, 0, 0));
-        normals.Add(new Vector3(+1, 0, 0));
-        normals.Add(new Vector3(+1, 0, 0));
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 0));
-        tris.Add(12);
-        tris.Add(13);
-        tris.Add(14);
-        tris.Add(14);
-        tris.Add(15);
-        tris.Add(12);
-
-        //Top
-        verts.Add(new Vector3(-0.5f, 1, -0.5f));
-        verts.Add(new Vector3(-0.5f, 1, +0.5f));
-        verts.Add(new Vector3(+0.5f, 1, +0.5f));
-        verts.Add(new Vector3(+0.5f, 1, -0.5f));
-        normals.Add(new Vector3(0, +1, 0));
-        normals.Add(new Vector3(0, +1, 0));
-        normals.Add(new Vector3(0, +1, 0));
-        normals.Add(new Vector3(0, +1, 0));
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 0));
-        tris.Add(16);
-        tris.Add(17);
-        tris.Add(18);
-        tris.Add(18);
-        tris.Add(19);
-        tris.Add(16);
-
-        //Bottom
-        verts.Add(new Vector3(-0.5f, 0, -0.5f));
-        verts.Add(new Vector3(+0.5f, 0, -0.5f));
-        verts.Add(new Vector3(+0.5f, 1, +0.5f));
-        verts.Add(new Vector3(-0.5f, 0, +0.5f));
-        normals.Add(new Vector3(0, -1, 0));
-        normals.Add(new Vector3(0, -1, 0));
-        normals.Add(new Vector3(0, -1, 0));
-        normals.Add(new Vector3(0, -1, 0));
-        uvs.Add(new Vector2(0, 0));
-        uvs.Add(new Vector2(0, 1));
-        uvs.Add(new Vector2(1, 1));
-        uvs.Add(new Vector2(1, 0));
-        tris.Add(20);
-        tris.Add(21);
-        tris.Add(22);
-        tris.Add(22);
-        tris.Add(23);
-        tris.Add(20);
-
-
-
-        foreach (Vector3 pos in verts)
-        {
-
-
-            Color color = Color.HSVToRGB(.7f, .6f, .7f);
-
-            colors.Add(color);
-        }
-
 
 
         foreach (Vector3 pos in verts)
@@ -607,6 +400,7 @@ public class CoralPurpleFan : MonoBehaviour
 
             colors.Add(color);
         }
+
 
         Mesh mesh = new Mesh();
         mesh.SetVertices(verts);
@@ -618,4 +412,3 @@ public class CoralPurpleFan : MonoBehaviour
 
     }
 }
-
