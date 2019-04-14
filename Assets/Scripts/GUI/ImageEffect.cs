69f58ee58e4ce22ba68d8dfa3f0d9864acd261af
Cameron Garchow <camerong124@gmail.com>
2019-04-14 14:50:06 -0500

    Under Water Shader Part 1
    +Water shader
    +Simplex noise
    +Added a package
    +Texture Added
    +Overlay Mat
    +Screen Script Added
    +Camera given feature
    =Edited SampleScene

---
+++ Assets/Scripts/GUI/ImageEffect.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode, ImageEffectAllowedInSceneView]
+public class ImageEffect : MonoBehaviour
+{
+    public Material material;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnRenderImage(RenderTexture src, RenderTexture dst)
+    {
+        //     material.SetFloat("_WarpAmount", Mathf.Sin(Time.time);
+        Graphics.Blit(src, dst, material);
+    }
+}
