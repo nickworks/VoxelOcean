7b6a3d4d3fcd79b6e598490e1cb44ae7811ce4e2
Cameron Garchow <camerong124@gmail.com>
2019-04-29 13:55:04 -0500

    Spawn fish
    Fixing up the fish to follow.

---
+++ Assets/Scripts/Creatures/CreatureCrystalFish.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatureCrystalFish : MonoBehaviour
+{
+
+    Vector3 acceleration;
+    Vector3 pos;
+    Vector3 velocity;
+    Vector3 target;
+    Vector3 offset;
+    Vector3 desired;
+    float offsetRotSp;
+    float r = 3.0f;
+    float MAX_FORCE = .05f;
+    float MAX_SPEED = 3f;
+    public static List<CreatureCrystalFish> glowfish = new List<CreatureCrystalFish>();
+    // Start is called before the first frame update
+    void Start()
+    {
+        acceleration = new Vector3(0, 0, 0);
+        velocity = new Vector3(Random.Range(-1,1), Random.Range(-1, 1), Random.Range(-1, 1));
+        pos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        flock();
+        velocity += acceleration;
+        if (velocity =< MAX_SPEED)
+        {
+
+        }
+        acceleration *= 0;
+    }
+
+    void seek(Vector3 target)
+    {
+        desired = Vector3(target.position.x, target.position.y, target.position.z);
+    }
+}
