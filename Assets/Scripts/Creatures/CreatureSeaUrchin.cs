463f591c2e586350c69cd434db218db2c6d1bbd0
Unknown <kaykberry@gmail.com>
2019-05-03 15:01:29 -0400

    Beginnings of changes to Urchin
    +changed urchin spawns

--- Assets/Scripts/Creatures/CreatureSeaUrchin.cs
+++ Assets/Scripts/Creatures/CreatureSeaUrchin.cs
@@ -5,11 +5,6 @@ using UnityEngine;
 public class CreatureSeaUrchin : MonoBehaviour
 {
     /// <summary>
-    /// The number of iterations spawned from the original
-    /// </summary>
-    public int iterations;
-    public int iteratoinsFan;
-    /// <summary>
     /// How the scaling of the branches are for both the urchin body and spines
     /// </summary>
     public Vector3 branchScaling = new Vector3(.5f, 2f, .5f);
@@ -25,32 +20,15 @@ public class CreatureSeaUrchin : MonoBehaviour
     /// </summary>
     public void Build()
     {
-        iterations = Random.Range(4, 5);
-        iteratoinsFan = iterations + 1;
+
         List<CombineInstance> meshes = new List<CombineInstance>();
         //What makes the spindles in the sea urchin
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-5, 5), 0, Random.Range(-25, 25)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-5, 5), 0, Random.Range(-40, 40)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-5,5), 0, Random.Range(-75, 75)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-5, 5), 0, Random.Range(-90, 90)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-30, 35), 0, Random.Range(-25, 25)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-30, 35), 0, Random.Range(-40, 40)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-30, 35), 0, Random.Range(-75, 75)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-30, 35), 0, Random.Range(-90, 90)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-75, 75), 0, Random.Range(-25, 25)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-75, 75), 0, Random.Range(-40, 40)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-75, 75), 0, Random.Range(-75, 75)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-75, 75), 0, Random.Range(-90, 90)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-90,90), 0, Random.Range(-25, 25)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-90, 90), 0, Random.Range(-40, 40)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-90, 90), 0, Random.Range(-75, 75)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-90, 90), 0, Random.Range(-90, 90)), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-5, 10), Random.Range(0, 180), 0), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-20, 25), Random.Range(0, 180), 0), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-30, 30), Random.Range(0, 180), 0), 1);
-        Grow(iterations, meshes, Vector3.zero, Quaternion.Euler(Random.Range(-90, 90), Random.Range(0, 180), 0), 1);
+        for (int i = 0; i < 30; i++) {
+            GrowSpines( meshes);
+        }
+     
         //creates the main body part of the urchin
-        Grow2(iteratoinsFan, meshes, Vector3.zero, Quaternion.identity, 1);
+        Grow2(meshes);
         //creates and combines the meshes created by the grow comands
         Mesh mesh = new Mesh();
         mesh.CombineMeshes(meshes.ToArray());
@@ -61,41 +39,29 @@ public class CreatureSeaUrchin : MonoBehaviour
     /// <summary>
     /// Where the grow function generates the new branches 
     /// </summary>
-    /// <param name="num">the number of iterations</param>
     /// <param name="meshes">list of the meshes we combined</param>
-    /// <param name="pos">position of our meshes</param>
-    /// <param name="rot">rotation of our meshes</param>
-    /// <param name="scale">scale of our meshes</param>
-    private void Grow(int num, List<CombineInstance> meshes, Vector3 pos, Quaternion rot, float scale)
+    private void GrowSpines(List<CombineInstance> meshes)
     {
-        //to help randomize put in this random number generator if its above 8 it will end it early so each spindle has a different amount of things.
 
-        if (num <= 0) return; //stop recursive function
-        num--;
         //adds meshes to the coral list that have been generated
         CombineInstance inst = new CombineInstance();
-
-        inst.mesh = MakeCube(num);
-        inst.transform = Matrix4x4.TRS(pos, rot, branchScaling * scale);
-
+        inst.mesh = MakeCube();
+        Vector3 dir = Random.insideUnitSphere;
+        if(dir.z> 0)
+        {
+            dir.z *= -1;
+        }
+        Quaternion rot = Quaternion.FromToRotation(Vector3.up, dir);
+        inst.transform = Matrix4x4.TRS(Vector3.one, rot, new Vector3(.1f,.1f,5));
         meshes.Add(inst);
-        //sets the scale of the urchins spindles.
-        scale *= .7f;
-        //sets a new position for the new spindle that will be spawning in
-        pos = inst.transform.MultiplyPoint(new Vector3(0, 1f, 0));
-
-        Grow(num, meshes, pos, rot, scale);
     }
-    private void Grow2(int num, List<CombineInstance> meshes, Vector3 pos, Quaternion rot, float scale)
+    private void Grow2( List<CombineInstance> meshes)
     {
        
-        if (num <= 0) return; //stop recursive function
-        num--;
         //adds meshes to the coral list that have been generated
         CombineInstance urchinBase = new CombineInstance();
-
         urchinBase.mesh = MakeCube1();
-        urchinBase.transform = Matrix4x4.TRS(pos, rot, baseScaling);
+        urchinBase.transform = Matrix4x4.TRS(new Vector3(.9f, .5f, 1f),Quaternion.identity, new Vector3(.8f, .8f, .8f));
 
 
         meshes.Add(urchinBase);
@@ -104,7 +70,7 @@ public class CreatureSeaUrchin : MonoBehaviour
     /// <summary>
     /// creates the mesh of the cube that is generated
     /// </summary>
-    private Mesh MakeCube(int num)
+    private Mesh MakeCube()
     {
 
         Mesh mesh = MeshTools.MakeTaperCube(.2f);
