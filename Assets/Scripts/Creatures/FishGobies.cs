a3ed19475c4afc443e5f96ee2d5b2092487fa259
Unknown <webbj19@ferris.edu>
2019-05-10 13:11:06 -0400

    updated fish gobbies
    updated script
    updated prefab

--- Assets/Scripts/Creatures/FishGobies.cs
+++ Assets/Scripts/Creatures/FishGobies.cs
@@ -7,10 +7,10 @@ using UnityEngine;
 public class FishGobies : MonoBehaviour
 {
     public GameObject FishGobbiesPrefab;// crates the gameobject
-    static int numGobbies = 0;// sets the number of objects created
-    public static int biomeSize = 50; // the range of how far they can go
-    public int spawnRadius = 1;
-   // public static Vector3 goalPos = Vector3.zero; // makes postion of object availlble
+    static int numGobbies = 25;// sets the number of objects created
+    public static int biomeSize = 5; // the range of how far they can go
+    public int spawnRadius = 5;
+    // public static Vector3 goalPos = Vector3.zero; // makes postion of object availlble
     public static GameObject[] fish = new GameObject[numGobbies];//the array of objects so they can see each other
     // Start is called before the first frame update
     /// <summary>
@@ -18,32 +18,31 @@ public class FishGobies : MonoBehaviour
     /// </summary>
     void Start()
     {
-       
+
         SpawnGobbies();
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+
+    }
     
-    }
-    public void NumGobbies()
-    {
-        numGobbies += Random.Range(2, 20);
-    }
     /// <summary>
     /// instantiate the objects so they will spawn in a random location based of the spawn radious in the x,y,z planes
     /// </summary>
     public void SpawnGobbies()
     {
 
-     
-            for (int i = 0; i < numGobbies; i++)
-            {
-                Vector3 pos = new Vector3(Random.Range(-spawnRadius, spawnRadius), Random.Range(-spawnRadius, spawnRadius), Random.Range(-spawnRadius, spawnRadius));
-                fish[i] = (GameObject)Instantiate(FishGobbiesPrefab);
-            }
-        
-    }
 
+        for (int i = 0; i < numGobbies; i++)
+        {
+            Vector3 pos = new Vector3(Random.Range(-spawnRadius, spawnRadius), Random.Range(-spawnRadius, spawnRadius), Random.Range(-spawnRadius, spawnRadius));
+            fish[i] = (GameObject)Instantiate(FishGobbiesPrefab);
+        }
+
+
+
+    }
 }
--- Assets/Scripts/Creatures/GobbieFlock.cs
+++ Assets/Scripts/Creatures/GobbieFlock.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// this class makes the spheres/ fish move and roatate and use a flocking pattering to get them to stay next to each other
+/// this class makes the spheres/ fish move and ratate and use a flocking pattering to get them to stay next to each other
 /// if oustside of desgnaited range they will turn around
 /// </summary>
-public class GobbieFlock: MonoBehaviour 
+public class GobbieFlock : MonoBehaviour
 {
     public float velocity; // how fast objects move
     public float maxVelocity; // the max speed they can go
     public float velocityRotation;// how fast they turn
     public float maxVelocityRotation;// max speed they can turn
-    
-    float neighbourDistance = .10f;// max distance an object can affect another object
+
+    float neighbourDistance = .50f;// max distance an object can affect another object
     bool turning = false;// used if object need to turn around
                          // Start is called before the first frame update
-/// <summary>
-/// calls the class to set a random speend for every object
-/// </summary>
+                         /// <summary>
+                         /// calls the class to set a random speend for every object
+                         /// </summary>
     void Start()
     {
-      RandomSpeed();
+        RandomSpeed();
+        //ZLocation();
     }
     /// <summary>
     /// use all the classes to make the object move
@@ -33,25 +34,33 @@ public class GobbieFlock: MonoBehaviour
         Swimming();
         Turning();
     }
+    public void ZLocation()
+    {
+        transform.Translate(Random.Range(1,11), 0, 0);
+    }
     /// <summary>
     /// makes the object move
     /// </summary>
     public void Swimming()
     {
-    
+        // if (velocity < maxVelocity)
+        // {
+        //      velocity += 0.2f;
+        // }
         transform.Translate(0, 0, velocity);
-       
+
     }/// <summary>
-    /// sets the random speeds of the objects
-    /// </summary>
+     /// sets the random speeds of the objects
+     /// </summary>
     public void RandomSpeed()
     {
-      velocity = Random.Range(-2.5f, 2.5f) * Time.deltaTime;
-  velocityRotation = Random.Range(1.1f, 1.5f) * Time.deltaTime;
+        velocity = Random.Range(1.0f, 5.5f) * Time.deltaTime;
+        velocityRotation = Random.Range(1.1f, 1.5f);
     }
     /// <summary>
-    /// sets the objects to follow each other bying taking all objects withen range and putting in group and yaking  the avearge of the group
-    /// when objects become to close they avoids each other
+    /// sets the objects to follow each other bying taking the avearge of the group
+    /// avoids each other
+    /// makes object head tworads a random set postion
     /// </summary>
     public void Flocking()
     {
@@ -60,67 +69,72 @@ public class GobbieFlock: MonoBehaviour
         Vector3 vCenter = Vector3.zero;
         Vector3 vAvoid = Vector3.zero;
 
-       
 
+        //Vector3 goalPos = FishGobies.goalPos;
         float distance;
         int groupSize = 0;
         foreach (GameObject go in fishGobbies)
         {
-            if(go != this.gameObject)
+            if (go != this.gameObject)
             {
                 distance = Vector3.Distance(go.transform.position, this.transform.position);
-                if(distance <= neighbourDistance)
+                if (distance <= neighbourDistance)
                 {
                     vCenter += go.transform.position;
                     groupSize++;
-                    if(distance < 0.1f)
+                    if (distance < 0.1f)
                     {
                         vAvoid = vAvoid + (this.transform.position - go.transform.position);
 
                     }
                     GobbieFlock anotherFlock = go.GetComponent<GobbieFlock>();
-                   
+
                 }
 
             }
         }
-        if(groupSize > 0)
+        if (groupSize > 0)
         {
-            
-       
+            //vCenter = vCenter / groupSize + (goalPos - this.transform.position);
+
             Vector3 direction = (vCenter + vAvoid) - transform.position;
-            if(direction != Vector3.zero)
+            if (direction != Vector3.zero)
             {
-                
-     
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation );
+
+                //      if (velocityRotation < maxVelocityRotation) {
+                //        velocityRotation += 0.3f;
+                //  }
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation * Time.deltaTime);
             }
         }
 
     }
     /// <summary>
     /// makes the objects turn around when they get outside the set size for the object
-    /// checks for a random number and if it is 100 object will turn
     /// </summary>
     public void Turning()
     {
-        int turn = Random.Range(1, 100);
-        if (Vector3.Distance(transform.position, Vector3.zero) >= FishGobies.biomeSize || turn == 100 )
+        if (Vector3.Distance(transform.position, Vector3.zero) >= FishGobies.biomeSize)
         {
             turning = true;
         }
         else
         {
             turning = false;
-         
+            // velocityRotation = 0;
         }
         if (turning == true)
         {
-
+            // if (velocityRotation < maxVelocityRotation) { 
+            // velocityRotation += 0.3f;
+            // }
             Vector3 direction = Vector3.zero - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation );
-
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation * Time.deltaTime);
+            //if (velocity > 0.1f)
+            // {
+            //    velocity -= 0.1f;
+            //}
         }
-        
+
     }
 }
