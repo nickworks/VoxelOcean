58f7857c41b4acad2a6e1dd407a9bf0d3447fa30
Eric Schrodt <edogschrodt@gmail.com>
2019-05-10 00:17:18 -0400

    UpdatedJelly2
    Added Comments

--- Assets/Scripts/Creatures/Jellyfish/Jellyfish.cs
+++ Assets/Scripts/Creatures/Jellyfish/Jellyfish.cs
@@ -4,12 +4,23 @@ using UnityEngine;
 
 public class Jellyfish : MonoBehaviour
 {
+    /// <summary>
+    /// The base speed that the Jellyfish move
+    /// </summary>
     public float speed = 1;
 
+    /// <summary>
+    /// The velocity of the jellyfish
+    /// </summary>
     Vector3 velocity;
+    /// <summary>
+    /// the acceleration of the jellyfish
+    /// </summary>
     Vector3 accel;
 
-    // Start is called before the first frame update
+    /// <summary>
+    /// Defines a random starting velocity of the jellyfish, and a random speed multiplier
+    /// </summary>
     void Start()
     {
         velocity = new Vector3(Random.Range(.25f,.5f), 0, Random.Range(.25f,.5f));
@@ -18,7 +29,9 @@ public class Jellyfish : MonoBehaviour
         speed *= Random.Range(.25f, .50f);
     }
 
-    // Update is called once per frame
+    /// <summary>
+    /// Determines movement
+    /// </summary>
     void Update()
     {
         velocity += accel * Time.deltaTime;
@@ -26,6 +39,10 @@ public class Jellyfish : MonoBehaviour
         transform.position = transform.position + velocity * Time.deltaTime;
     }
 
+    /// <summary>
+    /// Applies force to the jellyfish
+    /// </summary>
+    /// <param name="force"></param>
     void AddForce(Vector3 force)
     {
         accel += force;
