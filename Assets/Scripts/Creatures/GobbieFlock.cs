dace4fe214a551c2421c167e11be6e0024959d31
jesse <webbj19@ferris.edu>
2019-05-10 15:04:01 -0400

    fixed gobbies
    they move now

--- Assets/Scripts/Creatures/GobbieFlock.cs
+++ Assets/Scripts/Creatures/GobbieFlock.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 /// <summary>
-/// this class makes the spheres/ fish move and ratate and use a flocking pattering to get them to stay next to each other
+/// this class makes the spheres/ fish move and roatate and use a flocking pattering to get them to stay next to each other
 /// if oustside of desgnaited range they will turn around
 /// </summary>
 public class GobbieFlock : MonoBehaviour
@@ -12,7 +12,7 @@ public class GobbieFlock : MonoBehaviour
     public float velocityRotation;// how fast they turn
     public float maxVelocityRotation;// max speed they can turn
 
-    float neighbourDistance = .50f;// max distance an object can affect another object
+    float neighbourDistance = .10f;// max distance an object can affect another object
     bool turning = false;// used if object need to turn around
                          // Start is called before the first frame update
                          /// <summary>
@@ -21,7 +21,6 @@ public class GobbieFlock : MonoBehaviour
     void Start()
     {
         RandomSpeed();
-     
     }
     /// <summary>
     /// use all the classes to make the object move
@@ -30,23 +29,16 @@ public class GobbieFlock : MonoBehaviour
     void Update()
     {
 
-        Flocking();
+        //Flocking();
         Swimming();
         Turning();
-    }
-    public void ZLocation()
-    {
-        transform.Translate(Random.Range(1,11), 0, 0);
     }
     /// <summary>
     /// makes the object move
     /// </summary>
     public void Swimming()
     {
-        // if (velocity < maxVelocity)
-        // {
-        //      velocity += 0.2f;
-        // }
+
         transform.Translate(0, 0, velocity);
 
     }/// <summary>
@@ -54,13 +46,12 @@ public class GobbieFlock : MonoBehaviour
      /// </summary>
     public void RandomSpeed()
     {
-        velocity = Random.Range(1.0f, 5.5f) * Time.deltaTime;
-        velocityRotation = Random.Range(1.1f, 1.5f);
+        velocity = Random.Range(0.1f, 2.5f) * Time.deltaTime;
+        velocityRotation = Random.Range(1.1f, 1.5f) * Time.deltaTime;
     }
     /// <summary>
-    /// sets the objects to follow each other bying taking the avearge of the group
-    /// avoids each other
-    /// makes object head tworads a random set postion
+    /// sets the objects to follow each other bying taking all objects withen range and putting in group and yaking  the avearge of the group
+    /// when objects become to close they avoids each other
     /// </summary>
     public void Flocking()
     {
@@ -70,7 +61,7 @@ public class GobbieFlock : MonoBehaviour
         Vector3 vAvoid = Vector3.zero;
 
 
-        //Vector3 goalPos = FishGobies.goalPos;
+
         float distance;
         int groupSize = 0;
         foreach (GameObject go in fishGobbies)
@@ -95,45 +86,40 @@ public class GobbieFlock : MonoBehaviour
         }
         if (groupSize > 0)
         {
-            //vCenter = vCenter / groupSize + (goalPos - this.transform.position);
+
 
             Vector3 direction = (vCenter + vAvoid) - transform.position;
             if (direction != Vector3.zero)
             {
 
-                //      if (velocityRotation < maxVelocityRotation) {
-                //        velocityRotation += 0.3f;
-                //  }
-                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation * Time.deltaTime);
+
+                transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation);
             }
         }
 
     }
     /// <summary>
     /// makes the objects turn around when they get outside the set size for the object
+    /// checks for a random number and if it is 100 object will turn
     /// </summary>
     public void Turning()
     {
-        if (Vector3.Distance(transform.position, Vector3.zero) >= FishGobies.biomeSize)
+        int turn = Random.Range(1, 100);
+        if (Vector3.Distance(transform.position, Vector3.zero) >= FishGobies.biomeSize || turn == 100)
         {
             turning = true;
         }
         else
         {
             turning = false;
-            // velocityRotation = 0;
+
         }
         if (turning == true)
         {
-            // if (velocityRotation < maxVelocityRotation) { 
-            // velocityRotation += 0.3f;
-            // }
+
             Vector3 direction = Vector3.zero - transform.position;
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation * Time.deltaTime);
-            //if (velocity > 0.1f)
-            // {
-            //    velocity -= 0.1f;
-            //}
+            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), velocityRotation);
+
         }
 
     }
